0f14f910e1f25d5661257ca4d99e9388ae0430fc
rjames187 <rory.james2021@gmail.com>
2023-01-17 22:15:16 -0600

    lab 3b

---
+++ labs/3b/test_webserver.py
@@ -0,0 +1,45 @@
+import requests
+
+def test_home():
+    page = requests.get('http://localhost:5000')
+    true_content = '<h1>Hello!</h1><p>Congrats on building your own server!' +\
+        '</p><br /></br /><a href="/page2">Go to the next page</a>'
+
+    assert str(page.text) == true_content
+ 
+def test_page2():
+    page = requests.get('http://localhost:5000/page2')
+    true_content = '<h1>This is page 2!</h1><p>let us move to page three</p>' +\
+        '<br /></br /><a href="/page3">Go to the next page</a>'
+
+    assert str(page.text) == true_content
+
+def test_page3():
+    page = requests.get('http://localhost:5000/page3')
+    true_content = '<h1>This is page 3!</h1><p>And of end of our journey!</p>'
+
+    assert str(page.text) == true_content
+
+def test_hello_mike():
+    page = requests.get('http://localhost:5000/hello/mike')
+    true_content = '<h1>Hello mike</h1>'
+
+    assert str(page.text) == true_content
+
+def test_hello_sarah():
+    page = requests.get('http://localhost:5000/hello/sarah')
+    true_content = '<h1>Hello sarah</h1>'
+
+    assert str(page.text) == true_content
+
+def test_hello_crazy():
+    page = requests.get('http://localhost:5000/hello/crazy')
+    true_content = '<h1>Hello crazy</h1>'
+
+    assert str(page.text) == true_content
+
+def test_hello_asfdasdf():
+    page = requests.get('http://localhost:5000/hello/asfdasdf')
+    true_content = '<h1>Hello asfdasdf</h1>'
+
+    assert str(page.text) == true_content
