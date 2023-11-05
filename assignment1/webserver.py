3b79f6b5f21b5359a2f83e41a8165c353b7633f0
rjames187 <rory.james2021@gmail.com>
2023-11-05 16:53:33 -0600

    implmnt create task endpoint for assignment1

--- assignment1/webserver.py
+++ assignment1/webserver.py
@@ -1,2 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 import json
+
+app = Flask(__name__)
+
+cur_id = 1
+db = {
+  'tasks': []
+}
+
+@app.route('/v1/tasks', methods=['POST'])
+def create_task():
+  data = request.get_json()
+  task = {}
+  task['title'] = data['title']
+  task['is_completed'] = data['is_completed'] if 'is_completed' in data else False
+  task['id'] = cur_id
+  cur_id += 1
+  db['tasks'].append(task)
+  return { 'id': task['id'] }, 201
