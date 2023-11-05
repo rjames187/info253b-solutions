42692edd0b9d4bc06bbff48ba28f340ccb933693
rjames187 <rory.james2021@gmail.com>
2023-11-05 17:53:46 -0600

    refactor db to index ids for performance

--- assignment1/webserver.py
+++ assignment1/webserver.py
@@ -4,9 +4,7 @@ import json
 app = Flask(__name__)
 
 cur_id = 1
-db = {
-  'tasks': []
-}
+db = {}
 
 @app.route('/v1/tasks', methods=['POST'])
 def create_task():
@@ -15,27 +13,24 @@ def create_task():
   task['title'] = data['title']
   task['is_completed'] = data['is_completed'] if 'is_completed' in data else False
   task['id'] = cur_id
+  db[cur_id] = task
+  response = { 'id': cur_id }
   cur_id += 1
-  db['tasks'].append(task)
-  return { 'id': task['id'] }, 201
+  return response, 201
 
 @app.route('/v1/tasks', methods=['GET'])
 def list_tasks():
-  response = { 'tasks': db['tasks'] }
+  response = { 'tasks': list(db.values()) }
   return response, 200
 
 @app.route('/v1/tasks/<id>', methods=['GET'])
 def get_task(id):
-  for t in db['tasks']:
-    if t['id'] == id:
-      return t, 200
+  if id in db:
+    return db[id], 200
   return { 'error': 'There is no task at that id' }, 404
 
 @app.route('/v1/tasks/<id>', methods=['DELETE'])
 def delete_task(id):
-  for i in range(len(db['tasks'])):
-    t = db['tasks'][i]
-    if t['id'] == id:
-      db['tasks'].pop(i)
-      return None, 204
+  del db[id]
+  return None, 204
       
