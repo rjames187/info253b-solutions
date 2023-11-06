4a1e70fe41882ebdfac5bfc26e0934cb1f9d09f1
rjames187 <rory.james2021@gmail.com>
2023-11-05 22:40:51 -0600

    add tests for bulk create and delete in assignment1

--- assignment1/test_webserver.py
+++ assignment1/test_webserver.py
@@ -42,3 +42,39 @@ def test_get_missing_task():
 def test_edit_missing_task():
     r = requests.put('http://localhost:5000/v1/tasks/1', json={})
     assert r.status_code == 404
+
+def test_bulk_create_tasks():
+    data = {
+            "tasks": [
+                {"title": "Test Task 1", "is_completed": True},
+                {"title": "Test Task 2", "is_completed": False},
+                {"title": "Test Task 3", "is_completed": True}
+            ]
+        }
+    r = requests.post('http://localhost:5000/v1/tasks', json=data)
+    res = r.json()
+    assert isinstance(res, dict)
+    assert isinstance(res['tasks'], list)
+    assert isinstance(res['tasks'][0], dict)
+    assert isinstance(res['tasks'][0]['id'], int)
+    assert len(res['tasks']) == 3
+
+def test_list_all_tasks_again():
+    r = requests.get('http://localhost:5000/v1/tasks')
+    assert len(r.json()['tasks']) == 3
+
+def delete_bulk_tasks():
+    data = {
+        'tasks': [
+            { 'id': 1 },
+            { 'id': 2 },
+            { 'id': 3 }
+        ]
+    }
+    r = requests.delete('http://localhost:5000/v1/tasks', json=data)
+    assert not r.content
+
+def list_deleted_tasks():
+    r = requests.get('http://localhost:5000/v1/tasks')
+    assert len(r.json()['tasks']) == 0
+
