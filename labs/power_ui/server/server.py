9068b38c9f93a8529594d36f24a47017304f2b03
rjames187 <rory.james2021@gmail.com>
2023-12-11 17:34:30 -0600

    complete lab with cors handling and bug fixes

--- labs/power_ui/server/server.py
+++ labs/power_ui/server/server.py
@@ -4,18 +4,26 @@ app = Flask(__name__)
 
 @app.route('/add', methods=['GET'])
 def add():
-  data = request.get_json()
-  answer = data["num1"] + data["num2"]
+  num1 = int(request.args.get('num1'))
+  num2 = int(request.args.get('num2'))
+  answer = num1 + num2
   return { "answer": answer }
 
 @app.route('/sub', methods=['POST'])
 def sub():
-  data = request.get_json()
-  answer = data["num1"] - data["num2"]
+  data = request.form
+  answer = int(data["num1"]) - int(data["num2"])
   return { "answer": answer }
 
 @app.route('/mult', methods=['POST'])
 def mult():
   data = request.get_json()
-  answer = data["num1"] * data["num2"]
+  answer = int(data["num1"]) * int(data["num2"])
   return { "answer": answer }
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
