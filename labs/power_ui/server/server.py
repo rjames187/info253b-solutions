2d5df008646dcd1e832224ea5ffe630969ebbfd8
rjames187 <rory.james2021@gmail.com>
2023-12-11 17:25:46 -0600

    implement sub and mult endpoints for lab

--- labs/power_ui/server/server.py
+++ labs/power_ui/server/server.py
@@ -7,3 +7,15 @@ def add():
   data = request.get_json()
   answer = data["num1"] + data["num2"]
   return { "answer": answer }
+
+@app.route('/sub', methods=['POST'])
+def sub():
+  data = request.get_json()
+  answer = data["num1"] - data["num2"]
+  return { "answer": answer }
+
+@app.route('/mult', methods=['POST'])
+def mult():
+  data = request.get_json()
+  answer = data["num1"] * data["num2"]
+  return { "answer": answer }
