61222f635939fb85913bf3d95aa9b79b335c61fe
rjames187 <rory.james2021@gmail.com>
2023-12-18 15:17:30 -0600

    set up assignment 2

---
+++ assignment2/server.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+import json
+from model import database
+
+app = Flask(__name__)
+
+model = database()
+
+@app.route('/v1/tasks', methods=['POST'])
+def create_task():
+  data = request.get_json()
+  if not 'tasks' in data:
+    return model.add_task(data), 201
+  res = []
+  for t in data['tasks']:
+    res.append(model.add_task(t))
+  return { 'tasks': res }, 201
+
+@app.route('/v1/tasks', methods=['GET'])
+def list_tasks():
+  db = model.db
+  response = { 'tasks': list(db.values()) }
+  return response, 200
+
+@app.route('/v1/tasks/<id>', methods=['GET'])
+def get_task(id):
+  db = model.db
+  if id in db:
+    return db[id], 200
+  return { 'error': 'There is no task at that id' }, 404
+
+@app.route('/v1/tasks/<id>', methods=['DELETE'])
+def delete_task(id):
+  model.delete_task(id)
+  return {}, 204
+
+@app.route('/v1/tasks', methods=['DELETE'])
+def delete_bulk_task():
+  data = request.get_json()
+  for obj in data['tasks']:
+    model.delete_task(obj['id'])
+  return {}, 204
+
+
+@app.route('/v1/tasks/<id>', methods=['PUT'])
+def edit_task(id):
+  db = model.db
+  if not id in db:
+    return { 'error': 'There is no task at that id' }, 404
+  data = request.get_json()
+  t = db[id]
+  if 'title' in data:
+    t['title'] = data['title']
+  if 'is_completed' in data:
+    t['is_completed'] = data['is_completed']
+  return {}, 204
+      
