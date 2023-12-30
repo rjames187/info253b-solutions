aaa5d1366cc8c8522a0ebbffc75ed1116c37b0e2
rjames187 <rory.james2021@gmail.com>
2023-12-30 12:07:43 -0600

    implement email functionality of worker

--- assignment2/email-job/email_job.py
+++ assignment2/email-job/email_job.py
@@ -1,3 +1,19 @@
+import os
+import requests
+
 def send_email(address):
-  print('this is a placeholder:')
+  API_KEY = os.getenv('API_KEY')
+  DOMAIN = os.getenv('DOMAIN')
+  print(API_KEY, DOMAIN)
   print(address + ' is the address!')
+  requests.post(
+    f"https://api.mailgun.net/v3/{DOMAIN}/messages",
+    auth=("api", API_KEY),
+    data={"from": f"Task Application <mailgun@{DOMAIN}>",
+          "to": [address],
+          "subject": "Task Completed",
+          "text": "Congratulations on Completing a task!"
+          }
+  )
+  print('message sent to mailgun!')
+
